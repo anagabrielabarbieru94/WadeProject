410a978aaa08123c93d9c60374f29d9cfafeae6f
Samarithan <alexandru.vasiliu2017@gmail.com>
2018-12-30 21:07:02 +0200

    add basic of Zuul Proxy configuration

---
+++ Implementation/Zuul/src/test/java/wade/Zuul/ZuulConfigTest.java
@@ -0,0 +1,38 @@
+package wade.Zuul;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ZuulConfigTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ZuulConfigTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ZuulConfigTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
