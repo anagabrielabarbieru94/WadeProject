569ffa37227baec8dfbfd8cf03615d958995e5ac
Samarithan <alexandru.vasiliu2017@gmail.com>
2019-01-03 22:29:01 +0200

    Add dummy postgre database to Account Service

--- Implementation/account_service/src/main/java/wade/account_service/EurekaClientApplication.java
+++ Implementation/account_service/src/main/java/wade/account_service/EurekaClientApplication.java
@@ -4,22 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.netflix.discovery.EurekaClient;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import wade.controller.AccountController;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
-@RestController
 @EnableDiscoveryClient
-public class EurekaClientApplication implements GreetingController{
+@ComponentScan(basePackages = {"wade.repository", "wade.model"})
+@EnableJpaRepositories("wade.repository")
+@EntityScan(basePackages = "wade.model")
+public class EurekaClientApplication extends AccountController{
 	 @Autowired
 	 @Lazy
 	 private EurekaClient eurekaClient;
@@ -36,8 +36,4 @@ public class EurekaClientApplication implements GreetingController{
         return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
     }
 
-	@Override
-	public String mainPage() {
-		return "This is AccountService main page. Welcome!";
-	}
 }
