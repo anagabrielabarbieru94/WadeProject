dbfd6c60fd9f3ca849544f1e00ec56773c1e24a5
anagabrielabarbieru94 <anagabrielabarbieru@gmail.com>
2019-01-24 23:13:28 +0200

    Add script for geonames data extractions (hotels, restaurants, objectives)

---
+++ Implementation/sparql_endpoint/geonames_extract.py
@@ -0,0 +1,254 @@
+import geocoder
+
+sparqlEndpoint = "http://192.168.0.102:7200/repositories/towas/statements"
+
+
+def getGeoNamesHotels(city, countryCode):
+    print("\nHotels")
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    state = ""
+    for r in g:
+        state = r.state
+        break
+
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru', featureCode='HTL')
+    nearbyHotels = [r for r in g]
+    print(nearbyHotels)
+
+    addresses = [r.address for r in nearbyHotels]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru', featureCode='HTL')
+    proximityHotels = [r for r in g if r.address not in addresses]
+    print(proximityHotels)
+
+
+def getGeoNamesRestaurants(city, countryCode):
+    state = ""
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    for r in g:
+        state = r.state
+        break
+
+    print("\nRestaurants")
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru', featureCode='REST')
+    nearbyRestaurants = [r for r in g]
+    print(nearbyRestaurants)
+
+    addresses = [r.address for r in nearbyRestaurants]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru', featureCode='REST')
+    proximityRestaurants = [r for r in g if r.address not in addresses]
+    print(proximityRestaurants)
+
+def getGeoNamesMuseums(city, countryCode):
+    state = ""
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    for r in g:
+        state = r.state
+        break
+
+    print("\nMuseums")
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru', featureCode='MUS')
+    nearbyMuseums = [r for r in g]
+    print(nearbyMuseums)
+
+    addresses = [r.address for r in nearbyMuseums]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru', featureCode='MUS')
+    proximityMuseums = [r for r in g if r.address not in addresses]
+    print(proximityMuseums)
+
+def getGeoNamesMonuments(city, countryCode):
+    state = ""
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    for r in g:
+        state = r.state
+        break
+
+    print("\nMonuments")
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['MNMT', 'PYR', 'CSTL', 'PAL'])
+    nearbyMonuments = [r for r in g]
+    print(nearbyMonuments)
+
+    addresses = [r.address for r in nearbyMonuments]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['MNMT', 'PYR', 'CSTL', 'PAL'])
+    proximityMonuments = [r for r in g if r.address not in addresses]
+    print(proximityMonuments)
+
+def getGeoNamesTheaters(city, countryCode):
+    state = ""
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    state = ""
+    for r in g:
+        state = r.state
+        break
+
+    print("\nTheaters")
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru', featureCode=['THTR','OPRA'])
+    nearbyTheaters = [r for r in g]
+    print(nearbyTheaters)
+
+    addresses = [r.address for r in nearbyTheaters]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru', featureCode=['THTR','OPRA'])
+    proximityTheaters = [r for r in g if r.address not in addresses]
+    print(proximityTheaters)
+
+
+def getGeoNamesChurches(city, countryCode):
+    state = ""
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    for r in g:
+        state = r.state
+        break
+
+    print("\nChurches")
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['CH', 'MSTY', 'MSQE', 'TMPL'])
+    nearbyChurches = [r for r in g]
+    print(nearbyChurches)
+
+    addresses = [r.address for r in nearbyChurches]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['CH', 'MSTY', 'MSQE', 'TMPL'])
+    proximityChurches = [r for r in g if r.address not in addresses]
+    print(proximityChurches)
+
+def getGeoNamesMountains(city, countryCode):
+    state = ""
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    for r in g:
+        state = r.state
+        break
+
+    print("\nMountains")
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru', featureCode=['MT', 'MTS', 'PK', 'PKS'])
+    nearbyMountains = [r for r in g]
+    print(nearbyMountains)
+
+    addresses = [r.address for r in nearbyMountains]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['MT', 'MTS', 'PK', 'PKS'])
+    proximityMountains = [r for r in g if r.address not in addresses]
+    print(proximityMountains)
+
+def getGeoNamesBeaches(city, countryCode):
+    state = ""
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    for r in g:
+        state = r.state
+        break
+
+    print("\nBeaches")
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['BCH', 'BCHS', 'CAPE', 'SEA'])
+    nearbyBeaches = [r for r in g]
+    print(nearbyBeaches)
+
+    addresses = [r.address for r in nearbyBeaches]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['BCH', 'BCHS', 'CAPE', 'SEA'])
+    proximityBeaches = [r for r in g if r.address not in addresses]
+    print(proximityBeaches)
+
+def getGeoNamesLakes(city, countryCode):
+    state = ""
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    for r in g:
+        state = r.state
+        break
+
+    print("\nLakes")
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['LK', 'LKC', 'LKN', 'LKS', 'LKI', 'LKSC', 'LKX', 'RGNL'])
+    nearbyLakes = [r for r in g]
+    print(nearbyLakes)
+
+    addresses = [r.address for r in nearbyLakes]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['LK', 'LKC', 'LKN', 'LKS', 'LKI', 'LKSC', 'LKX', 'RGNL'])
+    proximityLakes = [r for r in g if r.address not in addresses]
+    print(proximityLakes)
+
+def getGeoNamesForests(city, countryCode):
+    state = ""
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    for r in g:
+        state = r.state
+        break
+
+    print("\nForests")
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['FRST', 'FRSTF', 'RESF'])
+    nearbyForests = [r for r in g]
+    print(nearbyForests)
+
+    addresses = [r.address for r in nearbyForests]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['FRST', 'FRSTF', 'RESF'])
+    proximityForests = [r for r in g if r.address not in addresses]
+    print(proximityForests)
+
+def getGeoNamesReservations(city, countryCode):
+    state = ""
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    for r in g:
+        state = r.state
+        break
+
+    print("\nNatural reservations")
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['RESN', 'RESV', 'RESW', 'RES', 'PRK'])
+    nearbyReservations = [r for r in g]
+    print(nearbyReservations)
+
+    addresses = [r.address for r in nearbyReservations]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['RESN', 'RESV', 'RESW', 'RES', 'PRK'])
+    proximityReservations = [r for r in g if r.address not in addresses]
+    print(proximityReservations)
+
+def getGeoNamesEntertainment(city, countryCode):
+    state = ""
+    g = geocoder.geonames(city, country=[countryCode], key='gabibarbieru')
+    for r in g:
+        state = r.state
+        break
+
+    print("\nEntertainment")
+    g = geocoder.geonames(city, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['AMUS', 'RESV', 'MALL', 'RSRT', 'POOL']) #temathic parks, stadiums, malls
+    nearbyEntertainment = [r for r in g]
+    print(nearbyEntertainment)
+
+    addresses = [r.address for r in nearbyEntertainment]
+
+    g = geocoder.geonames(state, maxRows=1000, country=[countryCode], key='gabibarbieru',
+                          featureCode=['AMUS', 'RESV', 'MALL', 'RSRT', 'POOL'])
+    proximityEntertainment = [r for r in g if r.address not in addresses]
+    print(proximityEntertainment)
+
+
+# getGeoNamesHotels("Berlin", 'DE')
+# getGeoNamesRestaurants("Roma", 'IT')
+#
+# getGeoNamesMuseums("Paris", 'FR')
+# getGeoNamesTheaters("Milan", 'IT')
+# getGeoNamesMonuments("Piatra Neamt", 'RO')
+# getGeoNamesChurches("Istanbul", '')
+#
+# getGeoNamesMountains('Suceava', 'RO')
+# getGeoNamesBeaches("Athens", 'GR')
+# getGeoNamesLakes("Zurich", "CH")
+# getGeoNamesForests("Paris", 'FR')
+# getGeoNamesReservations("yellowstone", "US")
+# getGeoNamesEntertainment("Paris", "FR")
