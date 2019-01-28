5c178808851cea01885859e7cffc6f85d11d388f
Anda Buinoschi <anda.buinoschi@outlook.com>
2019-01-28 23:37:47 +0200

    added to the html the validators

--- Frontend project/towas/src/app/sign-up/sign-up.component.ts
+++ Frontend project/towas/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsernameValidators } from './username.validators';
+import { FirstNameValidators } from './firstName.validators';
+import { LastNameValidators } from './lastName.validators';
+import { PasswordValidators } from './password.validators';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,6 +12,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
   src_url: String;
+  form = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      UsernameValidators.cannotContainSpace
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.compose([
+        Validators.minLength(8),
+        PasswordValidators.patternValidator(/\d/, { hasNumber: true }),
+        PasswordValidators.patternValidator(/[A-Z]/, { hasCapitalCase: true }),
+        PasswordValidators.patternValidator(/[a-z]/, { hasSmallCase: true }),
+        PasswordValidators.patternValidator(/[_+\-=,.]/, { hasSpecialCharacters: true })
+      ])
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required
+    ]),
+    firstName: new FormControl('', [
+        Validators.required,
+        FirstNameValidators.cannotContainSpace
+    ]),
+    lastName: new FormControl('', [
+        Validators.required,
+        LastNameValidators.cannotContainSpace
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ])
+  })
+
+  get username(){
+    return this.form.get('username');
+  }
+
+  get password(){
+    return this.form.get('password');
+  }
+
+  get confirmPassword(){
+    return this.form.get('confirmPassword');
+  }
+
+  get firstName(){
+    return this.form.get('firstName');
+  }
+
+  get lastName(){
+    return this.form.get('lastName');
+  }
+
+  get email(){
+    return this.form.get('email');
+  }
 
   constructor() { }
 
